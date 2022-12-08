9c0977766e5166bd1fa22a922898457a5141c92a
liuyuzhen <237925240@qq.com>
2022-12-08 21:42:29 +0800

    download data

---
+++ data/download.ps1
@@ -0,0 +1,15 @@
+curl -O "https://vision.maths.lth.se/calledataset/Alcatraz_water_tower/water_tower.zip"
+curl -O "https://vision.maths.lth.se/calledataset/kronan/kronan.zip"
+curl -O "https://vision.maths.lth.se/calledataset/fine_arts_palace/fine_arts_palace.zip"
+curl -O "https://vision.maths.lth.se/calledataset/plaza_de_armas/plaza_de_armas.zip"
+curl -O "https://vision.maths.lth.se/calledataset/Buddah_tooth_relic_temple/Buddah_tooth_relic_temple.zip"
+curl -O "https://vision.maths.lth.se/calledataset/Sri_Thendayuthapani/Sri_Thendayuthapani.zip"
+curl -O "https://vision.maths.lth.se/calledataset/Fort_Channing_gate/Fort_Channing_gate.zip"
+
+unzip -o water_tower.zip -d img/water_tower
+unzip -o kronan.zip -d img/kronan
+unzip -o fine_arts_palace.zip -d img/fine_arts_palace
+unzip -o plaza_de_armas.zip -d img/plaza_de_armas
+unzip -o Buddah_tooth_relic_temple.zip -d img/Buddah_tooth_relic_temple
+unzip -o Sri_Thendayuthapani.zip -d img/Sri_Thendayuthapani
+unzip -o Fort_Channing_gate.zip -d img/Fort_Channing_gate
